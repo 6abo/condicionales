db09b6225c06b0b3ecfe18998f64a98ae53019fe
Christian Alvarez <christian.alvarezr@ucuenca.edu.ec>
2024-09-30 16:07:38 -0500

    Se crea la pelota con movimiento mediante las teclas del teclado

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 Este es un proyecto para aprender condicionales.
 
-Adicionalmente hemos aprendido a usa CDN de p5.js incluyendo el siguiente codigo:
+Adicionalmente hemos aprendido a usa CDN de p5.js incluyendo el siguiente codigo dentro del `<head>`:
 Utilizamos el servicio cdn de p5.js, estos son distribuidores de codigo de p5 por lo que ya no tenemos que descargar la libreria
 
 ```
 <script src="https://cdn.jsdelivr.net/npm/p5@1.11.0/lib/p5.min.js"></script>
 
 ```
+
--- sketch.js
+++ sketch.js
@@ -1,7 +1,52 @@
+let ballX, ballY;  // Coordenadas de la pelota
+let ballRadius = 40;  // Radio de la pelota
+let ballSpeed = 8;  // Velocidad de movimiento de la pelota
+let bgColor;  // Color de fondo BackgroundColor
+
+// Función de configuración inicial de p5.js
 function setup() {
-    createCanvas(windowWidht, windowHeight);
-  }
-  
-  function draw() {
-    background(black);
-  }
+    createCanvas(windowWidth, windowHeight);
+    frameRate(60); // Cuadors por segundo
+    ballX = width / 2; // Posición inicial de la pelota en el centro (eje X)
+    ballY = height / 2; // Posición inicial de la pelota en el centro (eje Y)
+    bgColor = color(0); // Fondo negro al iniciar
+}
+
+// Función que se ejecuta continuamente para dibujar en el lienzo
+function draw() {
+    background(0); // Fondo blanco
+    drawBall(); // Dibujar la pelota
+    moveBall(); // Controlar el movimiento de la pelota con las teclas
+}
+
+
+function drawBall() {
+    fill(255, 165, 0); // Color naranja de la pelota
+    stroke(255); // borde de la pelota color
+    strokeWeight(2);
+    ellipse(ballX, ballY, ballRadius * 2); // Dibujar una elipse (pelota)
+}
+
+// Función para mover la pelota utilizando las teclas de flecha
+function moveBall() {
+    if (keyIsDown(LEFT_ARROW)) {
+        if (ballX - ballRadius > 0) { // Limitar movimiento al borde izquierdo
+            ballX -= ballSpeed;
+        }
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        if (ballX + ballRadius < width) { // Limitar movimiento al borde derecho
+            ballX += ballSpeed;
+        }
+    }
+    if (keyIsDown(UP_ARROW)) {
+        if (ballY - ballRadius > 0) { // Limitar movimiento al borde superior
+            ballY -= ballSpeed;
+        }
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        if (ballY + ballRadius < height) { // Limitar movimiento al borde inferior
+            ballY += ballSpeed;
+        }
+    }
+}
